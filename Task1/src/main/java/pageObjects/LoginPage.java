4ef36ac62ef56e45c55af9754f173a393bd776c0
mohamed mahran <mohamed_sabry007/2yahoo.com>
2020-02-23 20:11:33 +0200

    Fix issue in the login page

--- Task1/src/main/java/pageObjects/LoginPage.java
+++ Task1/src/main/java/pageObjects/LoginPage.java
@@ -1,17 +1,13 @@
 package pageObjects;
 
 import java.io.IOException;
-import javax.xml.parsers.ParserConfigurationException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.xml.sax.SAXException;
-
-import automationPracticeTests.BaseTest;
 import utilis.Helper;
 
-public class LoginPage extends BaseTest{
+public class LoginPage {
 
 
 
@@ -57,8 +53,8 @@ public class LoginPage extends BaseTest{
 
 	public void enterUserCredentialsAndSignIn() throws IOException {
 		Helper.loadFromPropertiesFile();
-		userEmailAddress.sendKeys(prop.getProperty("existingUserEmail"));
-		password.sendKeys(prop.getProperty("existingUserPassword"));
+		userEmailAddress.sendKeys(BasePage.prop.getProperty("existingUserEmail"));
+		password.sendKeys(BasePage.prop.getProperty("existingUserPassword"));
 		signin.click();
 
 	}
