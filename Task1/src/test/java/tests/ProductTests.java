10959732815f11a15a73a383aa153e320939e0a6
mohamed mahran <mohamed_sabry007/2yahoo.com>
2020-06-14 00:53:28 +0200

    enhance project structure

--- Task1/src/test/java/tests/ProductTests.java
+++ Task1/src/test/java/tests/ProductTests.java
@@ -13,21 +13,21 @@ import pages.product.Product;
 import utilis.ExtentTestManager;
 
 public class ProductTests extends BaseTest {
-	
+
 	@Test
-	public void validateProductDetails()  {
-		
+	public void validateProductDetails() {
+
 		Logger log = Logger.getLogger(ProductTests.class);
 		LoginPage loginPage = LoginPage.getLoginPage();
 		HeaderPage header = HeaderPage.getHeaderPage();
 		LandingPage landing = LandingPage.getLandingPage();
 		MainMenu menu = MainMenu.getMainMenuPage();
-		Product product =Product.getProductPage();
-		String[] expectedDisplayedColors = {"Orange","Blue"};
-		String[] expectedColumns = {"Compositions","Styles","Properties"};
-		String[] expectedProduct = {"Styles","Casual"};
+		Product product = Product.getProductPage();
+		String[] expectedDisplayedColors = { "Orange", "Blue" };
+		String[] expectedColumns = { "Compositions", "Styles", "Properties" };
+		String[] expectedProduct = { "Styles", "Casual" };
 		String data = "Compositions";
-		String productItem ="Short Sleeve";
+		String productItem = "Short Sleeve";
 
 		ExtentTestManager.getTest().log(Status.INFO, " Log in as existing customer");
 		log.info("1. Log in as existing customer");
@@ -36,9 +36,7 @@ public class ProductTests extends BaseTest {
 		ExtentTestManager.getTest().log(Status.INFO, " 2. Click *Women* button in the header");
 		log.info("2. Click *Women* button in the header");
 		try {
-			loginPage.step().enterUserEmailAddress()
-					 		.enterPassword()
-					 		.signIn();
+			loginPage.step().enterUserEmailAddress().enterPassword().signIn();
 		} catch (IOException e) {
 
 			e.printStackTrace();
@@ -50,25 +48,17 @@ public class ProductTests extends BaseTest {
 		ExtentTestManager.getTest().log(Status.INFO, " 4. Click the product with name Faded Short Sleeve T-shirts");
 		log.info("4. Click the product with name Faded Short Sleeve T-shirts");
 		landing.step().clickOnProduct("Faded Short Sleeve T-shirts");
-		
-		
+
 		try {
-			product.check()	   .colorsOfTheProduct(expectedDisplayedColors)
-							   .countOfSocialSharingProduct()
-							   .columnCounts()
-							   .rowCount()
-							   .columnNames(expectedColumns)
-							   .rowDetails(expectedProduct)
-							   .presenceOfData(data)
-							   .productIsExist(productItem)
-							   .productDetails(productItem)
-							   .allData();
+			product.check().colorsOfTheProduct(expectedDisplayedColors).countOfSocialSharingProduct().columnCounts()
+					.rowCount().columnNames(expectedColumns).rowDetails(expectedProduct).presenceOfData(data)
+					.productIsExist(productItem).productDetails(productItem).allData();
 		} catch (Exception e) {
 
 			e.printStackTrace();
 		}
-		
+
 		header.step().signOut();
-	
+
 	}
 }
